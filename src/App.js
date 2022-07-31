9867f30f81bd5c4ae706fa417cd15bfec10e86d8
BeeFarsh <farsh@localhost.localdomain>
2022-07-31 14:11:38 -0400

    first_change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name : "max", age: 28},
+      {name : "maxima", age: 20},
+      {name : "maria", age: 29},
+    ],
+    otherstate : "some other state"
+  }
+
+  switchStateHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name : "maximilliano", age: 30},
+        {name : newName, age: 50},
+        {name : "madonna", age: 60}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name : "max", age: 30},
+        {name : "maxima", age: 50},
+        {name : event.target.value, age: 60}
+      ]
+    })
+  }
+  
+  render (){
+    const styles = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+    return (
+      <div className="App">
+        <h1>Hi, I am A react</h1>
+        <p>This is really working!!!</p>
+        <button onClick={() => this.switchStateHandler("mommy")}
+        style={styles}>Switch name</button>
+        <div>
+          <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+          <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
+          <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} 
+          click = {this.switchStateHandler.bind(this, "halimat")}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+        </div>
+      </div>
+    );
+    //properties means the atrributes we had to the function component
+    // return React.CreateElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"))
+  }
+    
+  
 }
 
 export default App;
+
+
+
