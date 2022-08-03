0755f42719cbad87b7b1e74096d03c1710b38cc4
BeeFarsh <farsh@localhost.localdomain>
2022-08-03 05:26:43 -0400

    change2

--- src/App.js
+++ src/App.js
@@ -5,31 +5,40 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name : "max", age: 28},
-      {name : "maxima", age: 20},
-      {name : "maria", age: 29},
+      {id: "a1", name : "max", age: 28},
+      {id: "a2", name : "maxima", age: 20},
+      {id: "a3", name : "maria", age: 29},
     ],
-    otherstate : "some other state"
+    otherstate : "some other state",
+    showname: false
   }
 
-  switchStateHandler = (newName) => {
-    this.setState({
-      persons: [
-        {name : "maximilliano", age: 30},
-        {name : newName, age: 50},
-        {name : "madonna", age: 60}
-      ]
+  // we updated this handler for the person into which input field we typed 
+  //now we are finding that one single person index by using the findindex() method we want to call
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(pInput => {
+      return pInput.id = id;
     })
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+
+    //now we need to update the array at the position 
+    const newperson = [...this.state.persons];
+    newperson[personIndex] = person
+
+
+    this.setState({persons: newperson})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name : "max", age: 30},
-        {name : "maxima", age: 50},
-        {name : event.target.value, age: 60}
-      ]
-    })
+  togglePersonsHandler = () => {
+    const doeshow = this.state.showname; 
+    this.setState({showname: !doeshow})
+  }
+
+  deletePersonsHandler = (personIndex) => {
+    const persons = [...this.state.persons];//another good way of doing this without mutating the objects is by using the SPLICE() method
+    persons.splice(personIndex, 1) //splicing the persons object here has already made me mutate the original data here
+    this.setState({persons: persons})
   }
   
   render (){
@@ -40,19 +49,30 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     }
+
+    //RENDERING OUR DYNAMIC CONTENT USING CONDITIONAL STATEMENT
+    let myperson = null;
+    if (this.state.showname === true) {
+      myperson = (
+        <div>
+          {this.state.persons.map((persons, index) => {
+            return <Person 
+            click = {() => {this.deletePersonsHandler(index)}}
+            name= {persons.name} 
+            age={persons.age}
+            key={persons.id}
+            changed ={(event) => this.nameChangedHandler(event, persons.id)}/>
+          })}
+        </div>
+      )
+    }
     return (
       <div className="App">
         <h1>Hi, I am A react</h1>
         <p>This is really working!!!</p>
-        <button onClick={() => this.switchStateHandler("mommy")}
-        style={styles}>Switch name</button>
-        <div>
-          <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-          <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
-          <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} 
-          click = {this.switchStateHandler.bind(this, "halimat")}
-          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-        </div>
+        <button onClick={this.togglePersonsHandler}
+        style={styles}>Toggle persons</button>
+        {myperson}
       </div>
     );
     //properties means the atrributes we had to the function component
